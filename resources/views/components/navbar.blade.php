c2bdfff7b5e6417a24b791e55a5463f80d7693e2
123ApplePie <iqgeniuslab@gmail.com>
2023-12-21 12:37:14 -0300

    Index page added

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="header">
+    <a href="" class="logo">Logo</a>
+
+    <input type="checkbox" name="" id="check">
+    <label for="check" class="icons">
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <i class='bx bx-x' id="close-icon"></i>
+    </label>
+
+    <nav class="navbar">
+        <a href="/" class="hover:text-[#f84525]" style="--i:0;">Home</a>
+        <a href="/blog" class="hover:text-[#f84525]" style="--i:1;">Blog</a>
+        <a href="" class="hover:text-[#f84525]" style="--i:2;">Over Ons</a>
+        <a href="/contact" class="hover:text-[#f84525]" style="--i:3;">Contact</a>
+        <a href="" class="bg-[#f84525] px-4 py-2 rounded-3xl" style="--i:4;"><i class='bx bx-plus-circle'></i> Registreren</a>
+    </nav>
+</header>
---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,5 @@
+<x-guest-layout>
+    <x-navbar/>
+
+    
+</x-guest-layout>
