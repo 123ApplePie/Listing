a3fabcfbfac74edc03085444e74d22d16fcea0f3
123ApplePie <iqgeniuslab@gmail.com>
2024-01-04 12:44:05 -0300

    Get only cities with companies

--- app/Livewire/LwCitytable.php
+++ app/Livewire/LwCitytable.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\City;
 use App\Models\Region;
+use App\Models\Company;
 
 class LwCitytable extends Component
 {
@@ -18,13 +19,27 @@ class LwCitytable extends Component
 
     public function render()
     {
-        // dd($this->region_code);
 
         if (empty($this->stad)) {
-            $cities = City::with('regio_rel')
+            $id = $this->region_code;
+
+            $companies = Company::with('city_rel')
+                                ->whereHas('city_rel', function($q) use($id){$q
+                                ->where('region', '=', $id);
+                                })
+                                ->orderBy('id', 'desc')
+                                ->get();
+    
+            foreach ($companies as $company) {
+                            $ids[] = $company['city_id'];   //Create an array of the city_id's with companies
+                    }
+    
+            $cities = City::whereIn('id',$ids)              //Get the city if the id is in the array
+                            ->with('regio_rel')
                             ->where('region',$this->region_code)
                             ->orderBy('accentcity','asc')
                             ->paginate(20);
+    
         } else {
             $cities = City::with('regio_rel')
                             ->where('region',$this->region_code)
