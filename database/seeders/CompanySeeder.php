76793a141dc9d3cdc588b3d2b29c43dde048b5de
123ApplePie <iqgeniuslab@gmail.com>
2024-01-04 12:23:03 -0300

    Faker and Seeder added for dummy data

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 2000) as $index) {
+            DB::table('companies')->insert([
+                'city_id' => $faker->numberBetween(2025412, 3174017),   //Interval of the cities
+                'company' => $faker->company,
+                'phone' => $faker->phoneNumber,
+                'email' => $faker->email,
+                'street' => $faker->streetName,
+            ]);
+        }    
+    }
+}
